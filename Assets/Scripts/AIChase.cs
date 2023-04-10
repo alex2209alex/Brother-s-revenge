48eb19b62bec7287e8a684f6ee801b2c6e3026c4
alex2209alex <92671388+alex2209alex@users.noreply.github.com>
2023-04-10 15:27:24 +0300

    Enemy follows player

--- Assets/Scripts/AIChase.cs
+++ Assets/Scripts/AIChase.cs
@@ -24,9 +24,12 @@ public class AIChase : MonoBehaviour
         direction.Normalize();
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-        if(aIMeleeAttack.attackDistance < distance && distance < distanceBetween) {
+        /*if(aIMeleeAttack.attackDistance < distance && distance < distanceBetween) {
             transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
             transform.rotation = Quaternion.Euler(Vector3.forward * angle);
-        }
+        }*/
+
+        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        transform.rotation = Quaternion.Euler(Vector3.forward * angle);
     }
 }
